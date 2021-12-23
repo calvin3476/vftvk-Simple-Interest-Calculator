fed32e0868054626aba9d01f624d5be54ea7a8de
Calvin Chang <calvin3476@gmail.com>
2021-12-23 05:19:27 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+	var principal = document.getElementById("principal").value;
+	var rate = document.getElementById("rate").value;
+
+	var years = document.getElementById("years").value;
+  
+	var interest = principal * years * rate /100;
+
+	var year = new Date().getFullYear()+parseInt(years);
+	
+	var amount=interest;
+	
+		
+	if (principal <= 0) {
+		
+		document.getElementById("result").innerHTML="Enter a positive number";
+		//document.getElementById("principal").focus();
+	} 
+	else
+	{
+    	
+	//document.getElementById("result").innerHTML="test";
+	    //document.getElementById("result").innerHTML=principal;
+	  
+		document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;at an interest rate of <mark>"+rate+"%</mark>\<br\>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You will receive an amount of <mark>"+amount+"</mark>,\<br\>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;in the year <mark>"+year+"</mark>\<br\>"
+		document.getElementById("principal").focus();
+	
+	}
+	document.getElementById("principal").focus();
 }
-        
